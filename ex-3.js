76e6d60494f67300c4bbb6162596930522b55190
MOGunyaluck <gunyaluck0619@gmail.com>
2025-12-12 11:11:11 +0700

    Add initial implementations for user greeting, temperature checks, and login validation

---
+++ ex-3.js
@@ -0,0 +1,10 @@
+let correctEmail = "johnza007@gmai.com";
+let correctPassword = "johnlovecoding555";
+let userEmail 
+let userPassword 
+
+if (userEmail === correctEmail && userPassword === correctPassword) {
+    console.log("ยินดีต้อนรับเข้าสู่ระบบ ✨");
+} else {
+    console.log("กรุณาตรวจสอบ Email หรือ Password ใหม่อีกครั้ง ❌");
+}
